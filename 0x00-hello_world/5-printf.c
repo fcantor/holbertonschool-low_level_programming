ac64ae501a20d296995d388391e47bd1cce544cb
Francesca Cantor <fpcantor@gmail.com>
2018-06-20 21:15:56 +0000

    Added file 5-printf.c which prints exactly the string 'with proper grammar, but the outcome is a piece of art,' followed by a new line.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - The purpose of this file is to output
+ * the string: with proper grammar, but the outcome is a piece of art,
+ * followed by a newline, using printf and not puts.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
