9062528c6fc307a9364b519d57330a91b25afdd1
Francesca Cantor <fpcantor@gmail.com>
2018-06-22 03:06:30 +0000

    Edited file 10 to removed the int typecasting, which was unnecessary

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the numbers from 00 to 99.
+ * Numbers are separated by a comma and a space, are printed
+ * in ascending order, and putchar is used to print to screen.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int a, b;
+
+        for(b = '0'; b <= '9'; b++)
+        {
+                for (a = '0'; a <= '9'; a++)
+                {
+                        putchar(b);
+                        putchar(a);
+                        if (b != '9' || a != '9')
+                        {
+                                putchar(',');
+                                putchar(' ');
+                        }
+
+                }
+        }
+
+        putchar('\n');
+
+        return (0);
+}
