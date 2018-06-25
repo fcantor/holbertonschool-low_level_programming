543315cae27a49653cded1354766c686888bed80
Francesca Cantor <fpcantor@gmail.com>
2018-06-25 18:59:33 +0000

    Edited file 101 to pass Betty coding style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-	      , 60);
+	write(2, "and that piece of art is useful\"", 33);
+	write(2, "- Dora Korpar, 2015-10-19\n", 26);
 	return (1);
 }
