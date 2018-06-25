d67b65f3060e1cfbcc7c476248657777cb2bdcc7
Francesca Cantor <fpcantor@gmail.com>
2018-06-25 19:14:49 +0000

    Edited file 101 comments to say that it returns 1 for success

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - This program prints a sentence and a date, followed
  * by a new line, to the standard error
- * Return: Always 0 (Success)
+ * Return: 1 for success
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\"", 33);
-	write(1, " - Dora Korpar, 2015-10-19\n", 27);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	      , 59);
 	return (1);
 }
