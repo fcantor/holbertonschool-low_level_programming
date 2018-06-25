caf28a58505f1b43bd1f683c38f33798312c9f6b
Francesca Cantor <fpcantor@gmail.com>
2018-06-25 19:02:48 +0000

    Added extra space to fix output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\"", 33);
-	write(2, "- Dora Korpar, 2015-10-19\n", 26);
+	write(2, " - Dora Korpar, 2015-10-19\n", 27);
 	return (1);
 }
