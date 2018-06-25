2a9402831ec7444175a3668dc7c825cf508dfcd8
Francesca Cantor <fpcantor@gmail.com>
2018-06-25 19:08:45 +0000

    Changed output of file 101 to 1 instead of 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\"", 33);
-	write(2, " - Dora Korpar, 2015-10-19\n", 27);
+	write(1, "and that piece of art is useful\"", 33);
+	write(1, " - Dora Korpar, 2015-10-19\n", 27);
 	return (1);
 }
