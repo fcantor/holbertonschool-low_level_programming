586de259e9dfa99d960c3d42a063f838ca68eae7
Francesca Cantor <fpcantor@gmail.com>
2018-06-26 19:14:15 +0000

    Added file 3-islower.c which is a function from holberton.h that checks whether the input char is a lowercase letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * _islower - checks whether the passed character is a lowercase letter
+ * @c: The character to check
+ *
+ * Return: 1 if c is lowercase, otherwise Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return 1;
+	else
+		return 0;
+}
