99ccee11c6cb77872bffe4269c38992320c9dd9e
Francesca Cantor <fpcantor@gmail.com>
2018-06-27 01:39:36 +0000

    Edited file 2-print_alphabet_x10.c to add void argument in function definition

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
  * print_alphabet_x10 - This function prints the alphabet 10 times
  * in lowercase, followed by a newline
  */
-void print_alphabet_x10()
+void print_alphabet_x10(void)
 {
 	int ch, i;
 
