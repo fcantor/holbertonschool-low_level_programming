9d9f445dad69ead2975932a5e3188bfacd0edb8e
Francesca Cantor <fpcantor@gmail.com>
2018-06-27 00:17:11 +0000

    Added files for task 9: 9-main.c, 9-times_table.c, 9-times_table - This function prints the 9 times table, starting with 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * times_table - This function prints the 9 times table
+ * starting with 0
+ */
+void times_table(void)
+{
+
+	int x, y, a, b;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			a = (x * y) / 10;
+			b = (x * y) % 10;
+
+			if ((x * y) > 9)
+				_putchar(a + '0');
+
+			_putchar(b + '0');
+
+			/* Don't print commas after the 10th column */
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			/* Add an extra space if printing single digits */
+			if ((x * y) < 10)
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
