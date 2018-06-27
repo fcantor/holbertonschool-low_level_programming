df1cc5ccbef9a05277e3972a55159d6ead39aa1a
Francesca Cantor <fpcantor@gmail.com>
2018-06-27 00:58:07 +0000

    Added files for task 10: 10-main.c, 10-add.c, 10-add - This function adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * add - This function adds two integers and returns the result
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: the sum of the two operands
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
