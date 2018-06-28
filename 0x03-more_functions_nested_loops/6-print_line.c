d74542db972321f8071468777da6b99ed1903521
Francesca Cantor <fpcantor@gmail.com>
2018-06-28 20:28:26 +0000

    Added holberton.h library function 6-print_line.c - This function draws a straight line in the terminal

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_line - This function draws a straight line in the terminal
+ * @n: Number of times the _ should be printed
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+
+	_putchar('\n');
+}
