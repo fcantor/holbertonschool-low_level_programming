3e124ddb229f1352aaea8a2bbe23d2cd8b7c6b50
Francesca Cantor <fpcantor@gmail.com>
2018-06-28 21:52:53 +0000

    Added holberton.h library function 8-print_square.c - This function prints a square

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_square - This function prints a sqaure,
+ * followed by a newline
+ */
+void print_square(int size)
+{
+	int row, column;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (column = 0; column < size; column++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+
+}
