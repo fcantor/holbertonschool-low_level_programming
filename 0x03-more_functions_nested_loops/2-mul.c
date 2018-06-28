d7066ee6bd28e1c18ec01daedcecb3018ca09d9e
Francesca Cantor <fpcantor@gmail.com>
2018-06-28 17:34:43 +0000

    Added holberton.h library function 2-mul.c - This function multiplies two integers

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - This function multiplies two integers
+ * @a: The first operand
+ * @b: The second operand
+ * Return: The product of both operands
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
