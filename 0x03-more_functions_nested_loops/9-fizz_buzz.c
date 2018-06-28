91935c11bd0574269959ddba4591890fcbf262cb
Francesca Cantor <fpcantor@gmail.com>
2018-06-28 22:13:07 +0000

    Added 9-fizz_buzz.c program - This program prints the number 1-100, but prints words for multiples of 3, 5, and 15

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the numbers 1-100, followed by a newline.
+ * But for multiples of 3, prints 'Fizz' instead of the number,
+ * for multiples of 5, prints 'Buzz', and for multiples of both,
+ * prints 'FizzBuzz'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%i ", i);
+	}
+
+	printf("\n");
+	return (0);
+}
