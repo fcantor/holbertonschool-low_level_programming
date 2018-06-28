6bea1e84f4a60d913dfc2bf3e966b3720ee3ca38
Francesca Cantor <fpcantor@gmail.com>
2018-06-28 22:15:47 +0000

    Added condition to print space only if i < 100

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,13 +14,16 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%i ", i);
+			printf("%i", i);
+
+		if (i != 100)
+			printf(" ");
 	}
 
 	printf("\n");
