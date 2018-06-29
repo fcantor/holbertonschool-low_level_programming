8be2145e9075df24f89a65beddb453c996d8a3b7
Francesca Cantor <fpcantor@gmail.com>
2018-06-29 06:24:58 +0000

    Added holberton.h library function 10-print_triangle.c - This function draws a triangle

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - This function prints a triangle
+ * followed by a newline
+ * @size: The size of the triangle
+ */
+void print_triangle(int size)
+{
+        int row, column, space, pound;
+
+        for (row = 0; row < size; row++)
+        {
+		column = size - 1 - row;
+                for (space = column; space > 0; space--)
+                {
+                        _putchar(' ');
+                }
+                for (pound = size - column; pound > 0; pound--)
+                {
+                        _putchar(35);
+                }
+                _putchar('\n');
+        }
+}
