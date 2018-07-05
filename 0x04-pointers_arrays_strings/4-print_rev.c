5b5b6ba1667eaed8eb4043427fca02be2dd15df7
Francesca Cantor <fpcantor@gmail.com>
2018-07-05 04:12:48 +0000

    Refactored code to add char variable

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,14 @@
 void print_rev(char *s)
 {
 	int i;
+	char ch;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != 0; i++)
 		;
-
-	for (; i >= 0; i--)
-		_putchar(s[i]);
-
+	for (i = i - 1; i >= 0; i--)
+	{
+		ch = s[i];
+		_putchar(ch);
+	}
 	_putchar('\n');
 }
