e3bbfe897820bf1c7186496150af4887d9d9c540
Francesca Cantor <fpcantor@gmail.com>
2018-07-05 06:03:23 +0000

    Fixed typo error

--- 0x04-pointers_arrays_strings/101-keygen.c
+++ 0x04-pointers_arrays_strings/101-keygen.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	int i, random, sum;
-	char alphabet[63] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	char key[63] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 	char password[100];
 
 	srand(time(NULL));
@@ -20,14 +20,14 @@ int main(void)
 
 	while (sum < (2772 - 122))
 	{
-		r = rand() % 62;
-		password[i] = array[r];
+		random = rand() % 62;
+		password[i] = key[random];
 		sum = sum + password[i];
 		i++;
 	}
 
-	r = 2772 - sum;
-	password[i] = r;
+	random = 2772 - sum;
+	password[i] = random;
 	i++;
 	password[i] = '\0';
 	printf("%s", password);
