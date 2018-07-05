618094584ef8fe5576abad96815778ecec64c364
Francesca Cantor <fpcantor@gmail.com>
2018-07-05 02:00:12 +0000

    Added parameter description for pointer passed n

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -4,6 +4,7 @@
  * reset_to_98 - This function takes a pointer to an int
  * as parameter and updates the value it points to
  * to 98
+ * @n: pointer value to change
  */
 void reset_to_98(int *n)
 {
