aee41c81bfe2253d3a6a40b522b661f1606f4d47
Francesca Cantor <fpcantor@gmail.com>
2018-07-05 02:20:32 +0000

    Added holberton.h library function 2-strlen.c - This function returns the length of a string

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _strlen - This function returns the length of a string
+ * @s: Variable that contains a character array to be counted
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		continue;
+
+	return i;
+}
