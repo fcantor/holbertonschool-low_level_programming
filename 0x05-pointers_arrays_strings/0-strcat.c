a1f162d70c788a4fe72b1878ee9e4754b36f361e
Francesca Cantor <fpcantor@gmail.com>
2018-07-08 03:16:44 +0000

    Removed space before open parenthesis on lines 15, 19, and 25

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -12,15 +12,15 @@ char *_strcat(char *dest, char *src)
 	int len, i;
 
 	/* take the length of string dest */
-	for(len = 0; dest[len] != 0; len++)
+	for (len = 0; dest[len] != 0; len++)
 		;
 
 	/* append src string to end of dest string */
-	for(i = 0; src[i] != 0; i++)
+	for (i = 0; src[i] != 0; i++)
 	{
 		dest[len] = src[i];
 		len++;
 	}
 
-	return(dest);
+	return (dest);
 }
