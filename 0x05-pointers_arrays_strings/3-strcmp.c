96204e5809c87f65d8d42a6ef554652003abf24f
Francesca Cantor <fpcantor@gmail.com>
2018-07-08 14:07:09 +0000

    Changed loops and return to get desired outcome

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -7,24 +7,10 @@
  * Return: The integer difference between s1 and s2
  */
 
-int _strcmp(char *s1, char *s2)
+int _strcmp (char * s1, char * s2)
 {
-	int i, n;
-
-	/* loop through both strings while s1 is not null */
-	for (i = 0; s1[i] != 0; i++)
-	{
-		/* if s1 char is equal to s2 char, n equal 0 */
-		if (s1[i] == s2[i])
-			n = 0;
-		/* else if s1 char not equal to s2 char, get difference */
-		else if (s1[i] != s2[i])
-			n = *s1 - *s2;
-		/* else continue through the loop */
-		else
-			continue;
-	}
-
-	/* return difference */
-	return (n);
+	for(; *s1 == *s2; ++s1, ++s2)
+		if(*s1 == *s2)
+			return 0;
+	return (*s1 - *s2);
 }
