26cf9161d98d9db73b36ebedad964ff27a33115e
Francesca Cantor <fpcantor@gmail.com>
2018-07-08 05:03:17 +0000

    Fixed loops to get correct output

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,18 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	char *ch;
 	int i, n;
 
-	/* loop through both strings */
-	for (ch = s1; *ch; ++ch)
+	/* loop through both strings while s1 is not null */
+	for (i = 0; s1[i] != 0; i++)
 	{
-		if (s1 == s2)
+		/* if s1 char is equal to s2 char, n equal 0 */
+		if (s1[i] == s2[i])
 			n = 0;
-		else if (*s1 != *s2)
-			n = s1 - s2;
+		/* else if s1 char not equal to s2 char, get difference */
+		else if (s1[i] != s2[i])
+			n = *s1 - *s2;
+		/* else continue through the loop */
 		else
 			continue;
 	}
