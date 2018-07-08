f7c0e3a3800527c1c31928474fb8699e8d3434f7
Francesca Cantor <fpcantor@gmail.com>
2018-07-08 04:12:05 +0000

    Added to check that src isn't null in first loop, added second loop to null terminate everything after src[i] in second loop

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -13,10 +13,11 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	/* copy n bytes of src string to dest string */
-	for (i = 0; i < n; i++)
-	{
+	for (i = 0; i < n && src[i] != 0; i++)
 		dest[i] = src[i];
-	}
+
+	for(; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
