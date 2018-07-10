01983b2a9564e2197f872950a0fda1fbc693440a
Francesca Cantor <fpcantor@gmail.com>
2018-07-10 19:53:01 +0000

    Added return description

--- 0x06-pointers_arrays_strings/0-memset.c
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -5,6 +5,7 @@
  * @s: The array buffer to fill
  * @b: The constant char byte to fill the buffer with
  * @n: The number of bytes to fill
+ * Return: The pointer to the memory area s
  */
 
 char *_memset(char *s, char b, unsigned int n)
