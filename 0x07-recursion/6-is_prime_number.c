3cb0bd36d5224f57be9d0c23f10de67fd4ad95c0
Francesca Cantor <fpcantor@gmail.com>
2018-07-12 21:18:13 +0000

    Added comments

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -9,10 +9,13 @@
 
 int helper(int n, int i)
 {
+	/* if n is greater than or equal to 25 */
 	if (n >= (i * i))
 	{
+		/* skip it's multiples and even numbers after 5 */
 		if (n % i == 0 || n % (i + 2) == 0)
 			return (0);
+		/* check primes after 11 */
 		i = i + 6;
 	}
 	return (1);
@@ -31,10 +34,13 @@ int is_prime_number(int n)
 	/* if less than 1, not a prime */
 	if (n <= 1)
 		return (0);
+	/* 2 and 3 are prime */
 	else if (n <= 3)
 		return (1);
+	/* if factors of 2 or 3, not likely a prime */
 	else if (n % 2 == 0 || n % 3 == 0)
 		return (0);
+	/* check everything after 5 */
 	else
 		return (helper(n, 5));
 }
