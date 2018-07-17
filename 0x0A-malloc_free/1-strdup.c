6facc8f0aa5aad9522c7e17feffbf137be08d25b
Francesca Cantor <fpcantor@gmail.com>
2018-07-17 21:41:43 +0000

    Added _strdup function to holberton.h library

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - This function returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string given as a parameter
+ * @str: The string to copy
+ * Return: A pointer to the newly allocated space, NULL if it fails
+ */
+
+char *_strdup(char *str)
+{
+	char *copy;
+	int i;
+
+	/* check for if string is NULL */
+	if (str == NULL)
+		return (NULL);
+
+	copy = malloc(sizeof(str));
+	for (i = 0; str[i] != '\0'; i++)
+		copy[i] = str[i];
+
+	return (copy);
+}
