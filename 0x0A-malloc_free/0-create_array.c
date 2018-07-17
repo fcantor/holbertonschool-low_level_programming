a10895dbd89d8dfbe0597487f2f2ea8efcc0b451
Francesca Cantor <fpcantor@gmail.com>
2018-07-17 20:45:45 +0000

    Added condition for if size is zero

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -14,6 +14,10 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i = 0;
 
+	/* checker for size */
+	if (size == 0)
+		return NULL;
+
 	/* create an array with size of char size */
 	s = malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
