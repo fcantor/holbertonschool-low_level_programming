f22fbddc3128422744da8be23d7e83b3caa23cac
Francesca Cantor <fpcantor@gmail.com>
2018-07-23 22:50:04 +0000

    Added return statement to first conditional NULL, added newlines to printf statements

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -11,11 +11,11 @@ void print_dog(struct dog *d)
 {
 	/* if the pointer is NULL, don't do anything */
 	if (d == NULL)
-		;
+		return;
 
 	/* if an element of d is NULL, print nil. else, print element */
 	if (d->name == NULL)
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 
@@ -23,7 +23,7 @@ void print_dog(struct dog *d)
 	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 
