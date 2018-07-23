ad4c4a5be66fcea5d57f4f590573be5f440401ff
Francesca Cantor <fpcantor@gmail.com>
2018-07-23 20:04:47 +0000

    First commit

---
+++ 0x0D-structures_typedef/README.md
@@ -0,0 +1,8 @@
+# 0x0D. C - Structures, typedef
+## Task 0. Django
+File: *dog.h*
+
+This header file defines a new type ```struct dog``` with the following elements:
+* ```char *name```
+* ```float age```
+* ```char* owner```
---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - This struct creates a new type
+ * @name: First member, char pointer
+ * @age: Second member, float
+ * @owner: Third memeber, char pointer
+ *
+ * Description: This struct has three elements and is a new type dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
