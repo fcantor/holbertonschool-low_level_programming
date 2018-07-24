0432aa7257d49d2bd70d07719e0240a312a55cec
Francesca Cantor <fpcantor@gmail.com>
2018-07-24 01:12:58 +0000

    Reluctantly changed instance name to new_dog

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -13,17 +13,17 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	/* creating an instance of type dog_t struct called puppo */
-	dog_t *puppo = malloc(sizeof(struct dog));
+	dog_t *new_dog = malloc(sizeof(struct dog));
 
 	/* if malloc fails, return NULL */
-	if (puppo == NULL)
+	if (new_dog == NULL)
 		return (NULL);
 
 	/* initialize values of new struct to passed values */
-	puppo->name = name;
-	puppo->age = age;
-	puppo->owner = owner;
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
 
 	/* return pointer to new struct puppo */
-	return (puppo);
+	return (new_dog);
 }
