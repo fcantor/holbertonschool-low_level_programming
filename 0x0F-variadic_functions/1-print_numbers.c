bfcaa3c1f8a1b6fb5a38ec89acf0817102623dd1
Francesca Cantor <fpcantor@gmail.com>
2018-07-28 20:48:29 +0000

    Added function 1-print_numbers.c function for variadic_functions.h header

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,46 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_numbers - This function prints numnbers followed by a newline
+ * @separator: The string to be printed between numbers
+ * @n: The number of integers passed to the function
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	/* declare a list to store the variable arguments */
+	va_list ap;
+
+	/* counter variable declaration */
+	unsigned int i;
+
+	/* make sure there are more than 0 arguments */
+	if (n == 0)
+		exit(98);
+
+	/* initialize list */
+	va_start(ap, n);
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i == n - 1)
+		{
+			printf("%d\n", va_arg(ap, int));
+			break;
+		}
+		else
+		{
+			/* if separator is NULL, don't print it */
+			if (separator == NULL)
+				printf("%d ", va_arg(ap, int));
+			else
+				printf("%d%s ", va_arg(ap, int), separator);
+		}
+	}
+
+	/* free memory */
+	va_end(ap);
+}
