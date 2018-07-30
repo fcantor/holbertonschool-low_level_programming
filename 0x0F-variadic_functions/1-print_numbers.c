4c73b9fe2d91e7ede1f9f5d00e5f0857e1839f6d
Francesca Cantor <fpcantor@gmail.com>
2018-07-30 07:58:50 +0000

    Fixed n==0 to output newline and 2 to standard error

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -16,31 +16,29 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	/* counter variable declaration */
 	unsigned int i;
 
-	if (n)
+	if (n == 0)
+		write(2, "\n", sizeof(char));
+
+	/* initialize list */
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
 	{
-		/* initialize list */
-		va_start(ap, n);
-
-		for (i = 0; i <= n; i++)
+		if (i == n - 1)
 		{
-			if (i == n - 1)
-			{
-				printf("%d\n", va_arg(ap, int));
-				break;
-			}
-			else
-			{
-				/* if separator is NULL, don't print it */
-				if (separator == NULL)
-					printf("%d", va_arg(ap, int));
-				else
-					printf("%d%s", va_arg(ap, int), separator);
-			}
+			printf("%d\n", va_arg(ap, int));
+			break;
 		}
-
-		/* free memory */
-		va_end(ap);
+		else
+		{
+			/* if separator is NULL, don't print it */
+			if (separator == NULL)
+				printf("%d", va_arg(ap, int));
+			else
+				printf("%d%s", va_arg(ap, int), separator);
+		}
 	}
-	else
-		write(2, va_arg(ap, int), sizeof(int));
+
+	/* free memory */
+	va_end(ap);
 }
