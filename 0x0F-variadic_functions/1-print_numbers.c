19ebddeee04291da411ca058729a93b7c196222f
Francesca Cantor <fpcantor@gmail.com>
2018-07-30 07:37:25 +0000

    Refactored if condition

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -16,30 +16,26 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	/* counter variable declaration */
 	unsigned int i;
 
-	/* make sure there are more than 0 arguments */
+	/* check if n is NULL */
 	if (n == 0)
 		return;
 
 	/* initialize list */
 	va_start(ap, n);
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		if (i == n - 1)
-		{
-			printf("%d\n", va_arg(ap, int));
-			break;
-		}
-		else
-		{
-			/* if separator is NULL, don't print it */
-			if (separator == NULL)
-				printf("%d", va_arg(ap, int));
-			else
-				printf("%d%s", va_arg(ap, int), separator);
-		}
+		/* print integer */
+		printf("%d", va_arg(ap, int));
+
+		/* print separator while not second to last var */
+		if (i < (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
 
 	/* free memory */
 	va_end(ap);
+
+	/* newline at the end of file */
+	printf("\n");
 }
