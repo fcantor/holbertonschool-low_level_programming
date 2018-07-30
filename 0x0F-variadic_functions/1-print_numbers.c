12ae24405d86fa1fa35b8116ad7258e87e883210
Francesca Cantor <fpcantor@gmail.com>
2018-07-30 07:41:27 +0000

    Changed output of n==0 to print newline instead of return

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -16,26 +16,30 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	/* counter variable declaration */
 	unsigned int i;
 
-	/* check if n is NULL */
+	/* make sure there are more than 0 arguments */
 	if (n == 0)
-		return;
+		printf("\n");
 
 	/* initialize list */
 	va_start(ap, n);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		/* print integer */
-		printf("%d", va_arg(ap, int));
-
-		/* print separator while not second to last var */
-		if (i < (n - 1) && separator != NULL)
-			printf("%s", separator);
+		if (i == n - 1)
+		{
+			printf("%d\n", va_arg(ap, int));
+			break;
+		}
+		else
+		{
+			/* if separator is NULL, don't print it */
+			if (separator == NULL)
+				printf("%d", va_arg(ap, int));
+			else
+				printf("%d%s", va_arg(ap, int), separator);
+		}
 	}
 
 	/* free memory */
 	va_end(ap);
-
-	/* newline at the end of file */
-	printf("\n");
 }
