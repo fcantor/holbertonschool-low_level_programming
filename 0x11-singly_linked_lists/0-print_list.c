4fb849cdc0b9d9d43e27a53a10a65801291f8ea1
Francesca Cantor <fpcantor@gmail.com>
2018-08-03 06:44:40 +0000

    Added if-else statement inside while loop to check for null elements inside the linked list

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -10,14 +10,18 @@
 
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	int i = 0;
 
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
-		count++;
+		if (h->str == NULL || h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
 		h = h->next;
+		i++;
 	}
 
-	return (count);
+	return (i);
 }
