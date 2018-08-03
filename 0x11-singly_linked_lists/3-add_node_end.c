743fc564280239d0cd86764ea77eb82376946512
Francesca Cantor <fpcantor@gmail.com>
2018-08-03 07:53:51 +0000

    Added function add_node_end which adds a node to a linked list_t list at the end. Also updated lists.h header with prototype

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,65 @@
+#include <stddef.h>
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - This function returns the length of a string
+ * @s: Variable that contains a character array to be counted
+ * Return: Length of the string
+ */
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		continue;
+
+	return (i);
+}
+
+
+/**
+ * *add_node_end - This function adds a new node at the
+ * end of a linked list_t list
+ * @head: The beginning of the linked list
+ * @str: The string to duplicate into the new node
+ * Return: The address of the new element, or NULL if it failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	/* creating last pointer and assigning head to it */
+	list_t *last = *head;
+
+	/* create new list & allocate memory for it */
+	list_t *new = (list_t *)malloc(sizeof(list_t));
+
+	/* if malloc fails, return NULL */
+	if (new == NULL)
+		return (NULL);
+
+	/* assign data to new list */
+	new->str = strdup(str);
+	new->len = _strlen(str);
+
+	/* assign link of new node to NULL */
+	new->next = NULL;
+
+	/* if head is NULL, new list becomes head */
+	if (*head == NULL)
+	{
+		*head = new;
+		return (NULL);
+	}
+
+	/* traverse list to get to the last node */
+	while (last->next != NULL)
+		last = last->next;
+
+	/* change pointer of last node to new node */
+	last->next = new;
+
+	return (new);
+
+}
--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -23,7 +23,7 @@ int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 int _strlen(const char *s);
-char *_strdup(const char *str);
 list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif /* _LISTS_H_ */
