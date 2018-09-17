566e3f20eef8593568131d06113a479e6ea012cd
Francesca Cantor <fpcantor@gmail.com>
2018-09-17 05:21:22 +0000

    Updated header with prototype for task 2

--- 0x16-doubly_linked_lists/lists.h
+++ 0x16-doubly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -20,5 +21,6 @@ typedef struct dlistint_s
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 #endif /* _LISTS_H_ */
