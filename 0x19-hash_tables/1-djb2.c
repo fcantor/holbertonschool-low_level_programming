37d1cc3df05b6846ba5f77ffa468ddd9526ccab0
Francesca Cantor <fpcantor@gmail.com>
2018-10-11 22:09:00 +0000

    Added solution to taks 1

---
+++ 0x19-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * djb2 - This hash function is by Dan Bernstein and uses the magic of number
+ * 33 to hash the key string passed into it
+ * @str: The key to hash
+ * Return: The hashed value
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+	int c;
+
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return (hash);
+
+}
