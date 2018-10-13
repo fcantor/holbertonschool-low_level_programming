cadbef3da432c76ec2fb6561d5eaabfa77079222
Francesca Cantor <fpcantor@gmail.com>
2018-10-13 05:03:23 +0000

    Added prototype of helper function

--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -37,6 +37,7 @@ typedef struct hash_table_s
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+hash_node_t *add_node(const char *key, const char *value);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 #endif /* _HASH_TABLES_H_ */
