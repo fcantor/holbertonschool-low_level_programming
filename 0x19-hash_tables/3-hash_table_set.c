55235fe9ad751e5a7754b0c0702457212f38f74a
Francesca Cantor <fpcantor@gmail.com>
2018-10-14 23:16:15 +0000

    Removed helper function

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -1,36 +1,6 @@
 #include "hash_tables.h"
 
 /**
- * add_node - creates a new node in the hash table
- * @key: The key to add
- * @value: The corresponding value to add
- * Return: A pointer to the new node
- */
-hash_node_t *add_node(const char *key, const char *value)
-{
-	hash_node_t *new_node;
-
-	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
-		return (NULL);
-	new_node->key = strdup(key);
-	if (!(new_node->key))
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->value = strdup(value);
-	if (!(new_node->value))
-	{
-		free(new_node->key);
-		free(new_node);
-		return (NULL);
-	}
-	new_node->next = NULL;
-	return (new_node);
-}
-
-/**
  * hash_table_set - This function adds an element to the hash table
  * @ht: A pointer to the hash table
  * @key: The key to add to the hash table
@@ -47,19 +17,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((const unsigned char *)key, ht->size);
 	p = ht->array[index];
-	for (; p != NULL; p = p->next)
+	if (p && strcmp(key, p->key) == 0)
 	{
-		if (strcmp(key, p->key) == 0)
-		{
-			free(p->value);
-			p->value = strdup(value);
-			return (1);
-		}
+		p->value = strdup(value);
+		return (1);
 	}
-
-	new_node = add_node(key, value);
+	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
 	new_node->next = p;
 	p = new_node;
 	return (1);
