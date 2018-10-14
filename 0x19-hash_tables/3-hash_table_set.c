925399c417a348338101348944e504dff21b253f
Francesca Cantor <fpcantor@gmail.com>
2018-10-14 22:21:36 +0000

    Added parenthesis to ! values

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -14,13 +14,13 @@ hash_node_t *add_node(const char *key, const char *value)
 	if (!new_node)
 		return (NULL);
 	new_node->key = strdup(key);
-	if (!new_node->key)
+	if (!(new_node->key))
 	{
 		free(new_node);
 		return (NULL);
 	}
 	new_node->value = strdup(value);
-	if (!new_node->value)
+	if (!(new_node->value))
 	{
 		free(new_node->key);
 		free(new_node);
