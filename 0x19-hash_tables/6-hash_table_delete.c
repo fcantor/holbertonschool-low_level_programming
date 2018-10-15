38cc3eb4ef987a4c3b3acee21064e637fcf38fc2
Francesca Cantor <fpcantor@gmail.com>
2018-10-15 05:09:50 +0000

    Added solution to task 6 and updated library with prototype

---
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - This function deletes a hash table
+ * @ht: The hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int idx = 0;
+	hash_node_t *p = NULL, *tmp = NULL;
+
+	while (i < ht->size)
+	{
+		p = ht->array[idx];
+		while (p)
+		{
+			tmp = p;
+			p = p->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+		idx++;
+	}
+	free(ht->array);
+	free(ht);
+}
