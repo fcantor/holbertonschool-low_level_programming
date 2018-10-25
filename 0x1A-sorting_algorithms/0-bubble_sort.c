7daa19cc37ad683286a48b4a230e0216c3a4738f
ceecurvin <ccurvi1@lsu.edu>
2018-10-25 21:38:57 +0000

    Order of operations for incrementing

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -9,22 +9,21 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int i, temp, flag;
+	int temp, flag;
+	size_t i = 0;
 
-	i = 0;
-
-	while (array != NULL)
+	while (array && i < size)
 	{
 		if (array[i] > array[i + 1])
 		{
-			flag += 1;
-			i += 1;
-
 			temp = array[i];
 			array[i] = array[i + 1];
 			array[i + 1] = temp;
 
 			print_array(array, size);
+
+			flag += 1;
+                        i += 1;
 		}
 		else
 			i += 1;
--- 0x1A-sorting_algorithms/print_array.c
+++ 0x1A-sorting_algorithms/print_array.c
@@ -21,24 +21,3 @@ void print_array(const int *array, size_t size)
 	}
 	printf("\n");
 }
-
-/**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
-void print_list(const listint_t *list)
-{
-	int i;
-
-	i = 0;
-	while (list)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", list->n);
-		++i;
-		list = list->next;
-	}
-	printf("\n");
-}
