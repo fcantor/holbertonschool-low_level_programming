58f0d8385bc7a952a8bb33189f603bb3ebdbcdd5
Francesca Cantor <fpcantor@gmail.com>
2018-10-25 20:26:14 +0000

    Added sort header and print functions

---
+++ 0x1A-sorting_algorithms/sort.h
@@ -0,0 +1,10 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+#endif /*_SORT_H_*/
