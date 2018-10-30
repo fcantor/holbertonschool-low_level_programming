960440253ef23a71790f28f47dfc0b5a948481f5
ceecurvin <ccurvi1@lsu.edu>
2018-10-30 19:22:51 +0000

    Feat; implementing selection sort

---
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ *selection_sort - sort an array of numbers using
+ *selection sort algorithm
+ *@array: array of integers
+ *@size: size of array
+ *Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j, small_num;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		small_num = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[small_num] > array[j])
+				small_num = j;
+		}
+		if (small_num != i)
+		{
+			temp = array[i];
+			array[i] = array[small_num];
+			array[small_num] = temp;
+
+			print_array(array, size);
+		}
+	}
+}
--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -22,5 +22,6 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
 
 #endif /*_SORT_H_*/
