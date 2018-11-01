2ef46fc29b114df852896578f89887dca2a85ea7
Francesca Cantor <fpcantor@gmail.com>
2018-11-01 02:28:01 +0000

    Added insertion sort list

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -1,46 +1,65 @@
 #include "sort.h"
 
 /**
- *
- *
+ * insertion_sort_list - Sorts a doubly linked list of integers in
+ * ascending order using the Insertion sort algorithm
+ * @list: double pointer to the doubly linked list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *head, *run1, *run2, *tmp;
-	int m = 0;
+	listint_t *run1 = *list;
+	listint_t *run2, *tmp_p, *tmp_n, *tmp;
 
-	run1 = head;
-	run2 = head->next;
+	run2 = (*list)->next;
 
 	/* run through linked list from left to right */
-	while (!run1)
+	while (run1 != NULL)
 	{
-		if (run2->n < run1->n)
+		if (run2 < run1)
 		{
 			/* swap */
-			m = run1->n;
-			run1->n = run2->n;
-			run2->n = m;
+			tmp_p = run1;
+			tmp_n = run1->next->next;
+			if (tmp_p)
+			{
+				run2->prev = tmp_p->prev;
+				run2->next = tmp_p;
+				tmp_p->prev = run2;
+				tmp_p->next = tmp_n;
+			}
+			if (tmp_n)
+				tmp_n->prev = tmp_p;
+			/* print list */
 			print_list(*list);
 		}
 
 		/* run through linked list from right to left */
-		while (run1 != head)
+		while (run1 != *list)
 		{
-			tmp = run1->prev->n;
-			if (run1->n < tmp->n)
+			if (run1 < run1->prev)
 			{
 				/* swap */
-				m = run1->n;
-				run1->n = tmp->n;
-				tmp->n = m;
+				tmp_p = run2->prev;
+				tmp_n = run2->next;
+				if (tmp_p)
+				{
+					run2->prev = tmp_p->prev;
+					run2->next = tmp_p;
+					tmp_p->prev = run2;
+					tmp_p->next = tmp_n;
+				}
+				if (tmp_n)
+                                        tmp_n->prev = tmp_p;
+                                if (run2->prev)
+                                        run2->prev->next = run2;
+				/* print list */
 				print_list(*list);
 			}
 			else
 			{
 				run1 = tmp;
-				if (tmp != head)
-					tmp->n = tmp->prev->n;
+				if (tmp != *list)
+					tmp = tmp->prev;
 			}
 		}
 		/* move runners */;
@@ -50,7 +69,7 @@ void insertion_sort_list(listint_t **list)
 			run2 = run2->next;
 		}
 		/* implement stop condition */
-		if (tmp == head)
+		if (run2->next == NULL)
 			break;
 	}
 }
