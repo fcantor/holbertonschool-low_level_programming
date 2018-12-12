1d3728a2cf2e96f7e2a6195e229ed6c3b9daecca
Francesca <fpcantor@gmail.com>
2018-12-12 15:45:23 -0800

    Fix: Moved malloc; assigned values to left and right

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -8,12 +8,16 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t new_node = malloc(sizeof(binary_tree_t));
+    binary_tree_t *new_node = NULL;
+
+    new_node = malloc(sizeof(binary_tree_t));
     if (!new_node)
 	return (NULL);
 
     new_node->parent = parent;
     new_node->n = value;
+    new_node->left = NULL;
+    new_node->right = NULL;
 
     return (new_node);
 }
