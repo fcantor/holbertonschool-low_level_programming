6e356f13fb6cf6a87372dd1b66a5baa14da7f05e
Francesca Cantor <fpcantor@gmail.com>
2018-12-13 21:58:30 +0000

    Fix: betty styling

--- 0x1C-binary_trees/3-binary_tree_delete.c
+++ 0x1C-binary_trees/3-binary_tree_delete.c
@@ -6,12 +6,12 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree)
-    {
-        if (tree->left)
-            binary_tree_delete(tree->left);
-        if (tree->right)
-            binary_tree_delete(tree->right);
-        free(tree);
-    }
+	if (tree)
+	{
+		if (tree->left)
+			binary_tree_delete(tree->left);
+		if (tree->right)
+			binary_tree_delete(tree->right);
+		free(tree);
+	}
 }
