50061b778b553351614751e41fa34e460180766b
Francesca Cantor <fpcantor@gmail.com>
2018-12-14 00:44:37 +0000

    Fix: Betty styling, added space

--- 0x1C-binary_trees/10-binary_tree_depth.c
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -11,7 +11,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	size_t depth = 0;
 
 	/* traverse up and count */
-	while(tree && tree->parent)
+	while (tree && tree->parent)
 	{
 		tree = tree->parent;
 		depth++;
