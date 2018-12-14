68f06278a2489bdb11a06f8558fabcabfe4dc350
Francesca Cantor <fpcantor@gmail.com>
2018-12-14 03:13:34 +0000

    Updated with prototype of task 15

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -46,5 +46,6 @@ size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
