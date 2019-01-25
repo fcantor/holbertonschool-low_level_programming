194cc330157856fcc0da6f5f1dc364faafc5b915
Francesca <fpcantor@gmail.com>
2019-01-25 01:21:01 +0000

    Changed condition in for loop

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array && size != 0)
 	{
-		for (; array[i] != '\0'; i++)
+		for (; i < size; i++)
 		{
 			printf("Value checked array[%d] = [%d]\n", i, array[i]);
 			if (array[i] == value)
